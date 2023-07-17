17d26abd6258c5dd67d8f1c2e2424c318f678883
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-07-17 16:57:04 +0200

    navbar icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="flex gap-x-16">
+      <Link
+        href="/"
+        className="flex items-center text-4xl hover:underline hover:underline-offset-8 transition hover:text-red-400/80"
+      >
+        <Image
+          src="/images/navbar/hacker-cat.svg"
+          width={69}
+          height={69}
+          alt="Home Icon"
+        />
+        Home
+      </Link>
+      <Link
+        href="/history"
+        className="flex items-center text-4xl hover:underline hover:underline-offset-8 transition hover:text-red-400/80"
+      >
+        <Image
+          src="/images/navbar/spider-web.svg"
+          width={69}
+          height={69}
+          alt="History Icon"
+        />
+        History
+      </Link>
+    </nav>
+  );
 }
