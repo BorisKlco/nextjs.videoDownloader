2f14690388f8ab0dbfd7ad366b44cf088818880f
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-07-19 17:47:55 +0200

    Toaster

--- components/Download.tsx
+++ components/Download.tsx
@@ -11,7 +11,7 @@ type DownloadProps = {
 export default function Download({ id, option, format = "" }: DownloadProps) {
   return (
     <a
-      onClick={() => toast.success("Preparing download file...")}
+      onClick={() => toast.success("Preparing... wait a few sec 🎧")}
       href={`http://127.0.0.1:8080/download?id=${id}&option=${option}`}
       className="flex items-center justify-center bg-button max-sm:w-[10rem] w-[12rem] h-[3rem] rounded-full font-semibold  border border-black transition hover:bg-logo"
     >
--- components/ToasterProvider.tsx
+++ components/ToasterProvider.tsx
@@ -8,16 +8,18 @@ export default function ToasterProvider() {
       toastOptions={{
         style: {
           border: "1px solid black",
-          padding: "0.5rem 1rem",
+          marginTop: "0.5rem",
+          padding: "0.75rem 1.5rem",
           fontSize: "1.25rem",
           color: "black",
           background: "#CDD5C6",
+          filter: "drop-shadow(0rem 0.1rem 0.5rem #ea5a47)",
         },
         iconTheme: {
           primary: "#ea5a47",
           secondary: "#EEE9D7",
         },
-        duration: 2500,
+        duration: 4000,
       }}
     />
   );
