3f0dc268fa9e7dfc959360254d7aa1fb5f1a02e0
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-07-19 13:17:36 +0200

    yt format check api

--- python/api.py
+++ python/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import yt_dlp
+import yt_formater
 
 app = Flask(__name__)
 CORS(app)
@@ -8,10 +9,11 @@ CORS(app)
 
 @app.route("/", methods=["GET"])
 def main_route():
-    return ""
+    test = yt_formater.yt_formater("https://www.youtube.com/watch?v=Gp8Knx9wdeA")
+    return jsonify(test)
 
 
-@app.route("/<video_id>", methods=["GET"])
+@app.route("/a/<video_id>", methods=["GET"])
 def video_json(video_id):
     with yt_dlp.YoutubeDL({}) as ydl:
         try:
---
+++ python/yt_formater.py
@@ -0,0 +1,34 @@
+from yt_dlp import YoutubeDL
+
+
+def yt_formater(url):
+    FORMATS = ["1080p60", "720p60", "1080p", "720p", "480p", "360p"]
+
+    def live_stream():
+        return {"error": "Error: Video is LiveStreamed!"}
+
+    def formats_check():
+        correct_formats = []
+        for resolution in FORMATS:
+            if len(correct_formats) < 2:
+                for format in info["formats"][::-1]:
+                    try:
+                        if (
+                            format["format_note"] == resolution
+                            and format["ext"] == "webm"
+                        ):
+                            correct_formats.append(format)
+                            break
+                    except:
+                        pass
+
+        return correct_formats
+
+    with YoutubeDL() as ydl:
+        info = ydl.extract_info(url, download=False)
+        ydl.sanitize_info(info)
+
+        if info["is_live"]:
+            return live_stream()
+        else:
+            return formats_check()
