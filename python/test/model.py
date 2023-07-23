15bb1685042f0689c4ca7d7f3e8c0b2fe47ca353
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-07-23 13:40:36 +0200

    Lil`cleaning

--- python/test/model.py
+++
@@ -1,6 +0,0 @@
-class History(db.Model):
-    id = db.Columns(db.Integer, primary_key=True)
-    url = db.Columns(db.String)
-    video = db.Columns(db.String)
-    audio = db.Columns(db.String)
-    data = db.Columns(db.JSONB)
--- python/tiktok_formater.py
+++
@@ -1,9 +0,0 @@
-from yt_dlp import YoutubeDL
-
-url = "https://www.tiktok.com/@stanleythestanman/video/7254736960290409770"
-
-with YoutubeDL() as ydl:
-    info = ydl.extract_info(url, download=False)
-    ydl.sanitize_info(info)
-
-    print(info)
